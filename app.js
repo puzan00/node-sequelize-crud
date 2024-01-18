b348b89621b660ce78fa03b738083889d23d5741
pujan shrestha <shresthapujan769@gmail.com>
2024-01-18 08:06:30 +0545

    some changes

--- app.js
+++ app.js
@@ -33,5 +33,29 @@ sequelize.sync().then(() => {
 });
 
 app.use((req, res) => {
-  res.status(404).send('404:This page could not be found');
+  res.status(404).send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>404 Error</title>
+      <style>
+        body {
+          text-align: center;
+          padding: 10%;
+          background-color:rgb(45, 42, 42);
+        }
+        h1 {
+          font-size: 3em;
+          color:white;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>404: This page could not be found</h1>
+    </body>
+    </html>
+  `);
 });
+
